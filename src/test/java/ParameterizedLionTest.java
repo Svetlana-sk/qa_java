7d0f142709a836363535665c45a0f2f9f2e03a52
Svetlana Bondarenko <sbondarenko@it-one.ru>
2023-12-20 16:26:52 +0000

    Revert "sprint_6_1"
    This reverts commit 72155534eceffa5528943b976efb9938a5d05a87.

--- src/test/java/ParameterizedLionTest.java
+++ src/test/java/ParameterizedLionTest.java
@@ -35,6 +35,7 @@ public class ParameterizedLionTest {
         return Arrays.asList(new Object[][]{
                 {"Самец", 3, true, List.of("Мясо"), false},
                 {"сОмец", 0, false, List.of(), true}
+                // Add more test cases as needed
         });
     }
 
